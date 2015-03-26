d6612eb4f5a587f4bd36857b21d8f13457a77a36
pardalgod <fabio.alves@ist.utl.pt>
2015-03-26 19:27:43 +0000

    File Manager created (and tested).
    Available Functions:
     - CreateFile
     - ReadFile
     - WriteFile
     - DeleteFile
     - ListDirectory (listDir)
     - getInternalFreeSpace (in Megabytes)

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/other/FileManager.java
@@ -0,0 +1,147 @@
+package pt.ulisboa.tecnico.cmov.airdesk.other;
+
+import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileManager {
+
+    /* MACROS */
+    // Line Separator
+    private static final String LINE_SEP = System.getProperty("line.separator");
+
+    /**
+     * Create File Function.
+     * Receives the filename and the path to which the file will be created on.
+     */
+    public static boolean createFile(String path, String filename){
+        try {
+            File file = new File(path, filename);
+
+            if(!file.exists()){
+                file.mkdirs();
+            }
+            Log.e("FileSystem", "File Location: " + file.getAbsolutePath());
+
+            return true;
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
+    /**
+     * List Directory Function.
+     * Receives the path of the directory and returns the files in it.
+     */
+    public static ArrayList<String> listDir(String path){
+        ArrayList<String> files = new ArrayList<>();
+        File file = new File(path);
+        if(file.exists()) {
+            File[] children = file.listFiles();
+            for (File f : children) {
+                files.add(f.getName());
+            }
+        }
+        else
+            Log.e("FileSystem", "File does not exist");
+        return files;
+    }
+
+    /**
+     * Read File Function.
+     * Receives the file's path, and returns the content of it.
+     */
+    public static String readFile(String path){
+        FileInputStream fis = null;
+        String content = null;
+        Scanner scanner = null;
+        File file = new File(path);
+        try {
+            fis = new FileInputStream(file);
+            scanner = new Scanner(fis);
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine() + LINE_SEP);
+            }
+            content = sb.toString();
+        } catch(Exception e){
+            Log.e("FileSystem","Couldn't open file " + file.getName());
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    Log.d("FileExplorer", "Close error.");
+                }
+            }
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        return content;
+    }
+
+    /**
+     * Write File Function.
+     * Receives the file's path and the content to be put, and write it over the file, replacing other existing content.
+     */
+    public static boolean writeFile(String path, String content){
+        FileOutputStream fos = null;
+        boolean returnValue = false;
+        try {
+            fos = new FileOutputStream(path);
+            fos.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            Log.e("FileSystem", "File not found", e);
+        } catch (IOException e) {
+            Log.e("FileSystem", "IO problem", e);
+        } finally {
+            try {
+                fos.close();
+                returnValue = true;
+            } catch (Exception e) {
+                Log.e("FileExplorer", "Close error.");
+            }
+        }
+        return returnValue;
+    }
+
+    /**
+     * Delete File Function.
+     * Receives the file's path, and delete it. If file is a directory, the function deletes all children first.
+     */
+    public static void deleteFile(String path){
+        File file = new File(path);
+        if(file.exists()){
+            if(file.isDirectory()){
+                File[] children = file.listFiles();
+                for(File f : children)
+                    deleteFile(f.getAbsolutePath());
+            }
+            file.delete();
+        }
+    }
+
+    /**
+     * Get Free Space Function.
+     * Returns the free space of the internal storage, in megabytes.
+     */
+    public static long getInternalFreeSpace(){
+        StatFs statFs = new StatFs(Environment.getDataDirectory().getAbsolutePath());
+        long free;
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+            free = statFs.getAvailableBytes() / 1048576;
+        else
+            free = ((long)statFs.getAvailableBlocks() * (long)statFs.getBlockSize()) / 1048576;
+        return free;
+    }
+}
