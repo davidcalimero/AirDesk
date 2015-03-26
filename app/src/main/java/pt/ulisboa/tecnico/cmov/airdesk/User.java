2e28cc87fc103325a943a450a961e680ac17ee9e
pardalgod <fabio.alves@ist.utl.pt>
2015-03-26 15:19:13 +0000

    Workspace and User Objects created.
    Need to treat:
     - Workspace:
       - Quota management, when setting it in the workspace;
       - User List;
    
     - User:
       -Keyword List;
    
     Eventually have deprecated code (such as the workspace's Public Profile, and the user's workspace list), correctly identified in the code.

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/User.java
@@ -0,0 +1,96 @@
+package pt.ulisboa.tecnico.cmov.airdesk;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class User {
+
+    /*********************************/
+    /*********** VARIABLES ***********/
+    /*********************************/
+
+    /* E-mail. Used as ID. */
+    private String _email;
+
+    /* Nickname */
+    private String _nickname;
+
+    /* Owned Workspace List */
+    private ArrayList<String> _workspaceList = new ArrayList<>();
+
+    /* Keywords List */
+    private ArrayList<String> _keywordList = new ArrayList<>();
+
+    /*********************************/
+    /********** CONSTRUCTOR **********/
+    /*********************************/
+
+    public User(String mail, String nick){
+        _email = mail;
+        _nickname = nick;
+    }
+
+    /*********************************/
+    /****** GETTERS AND SETTERS ******/
+    /*********************************/
+
+    /* Client ID. The client Email is the identifier.*/
+    public String getID(){
+        return _email;
+    }
+
+    /* Client Email */
+    public String getEmail() {
+        return _email;
+    }
+
+    /* Client Nickname */
+    public String getNickname() {
+        return _nickname;
+    }
+
+    /* Client Workspace List */
+    public ArrayList<String> getWorkspaceList() {
+        return _workspaceList;
+    }
+
+    /* Client Keyword List */
+    public ArrayList<String> getKeywordList() {
+        return _keywordList;
+    }
+
+    /*********************************/
+    /******** LIST MANAGEMENT ********/
+    /*********************************/
+
+    /* Keyword List */
+    public boolean removeKeyword(String keyword){
+        return _keywordList.remove(keyword);
+    }
+
+    public boolean addKeyword(String keyword){
+        try{
+            return _keywordList.add(keyword);
+        } catch (Exception e){
+            Log.e("User", "Can't add keyword. Had " + e.toString());
+            return false;
+        }
+    }
+
+    /* Workspace List */
+    //region DEPRECATED
+    public boolean removeWorkspace(String name){
+        return _workspaceList.remove(name);
+    }
+
+    public boolean addWorkspace(String name){
+        try{
+            return _workspaceList.add(name);
+        } catch (Exception e){
+            Log.e("User", "Can't add keyword. Had " + e.toString());
+            return false;
+        }
+    }
+    //endregion
+}
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Workspace.java
@@ -0,0 +1,119 @@
+package pt.ulisboa.tecnico.cmov.airdesk;
+
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Workspace {
+
+    /*********************************/
+    /************ MACROS *************/
+    /*********************************/
+
+    /* Privacy */
+    public enum MODE {PUBLIC, PRIVATE}
+
+    /*********************************/
+    /*********** VARIABLES ***********/
+    /*********************************/
+
+    /* Name of the workspace. */
+    private String _name = "";
+
+    /* User List */
+    private ArrayList<String> _users = new ArrayList<>();
+
+    /* Public Profile */
+    private ArrayList<String> _tags = new ArrayList<>();
+
+    /* Privacy */
+    private MODE _privacy = MODE.PRIVATE;
+
+    /* Quota */
+    private float _quota = 0;
+
+    /*********************************/
+    /********** CONSTRUCTOR **********/
+    /*********************************/
+
+    /* Workspace */
+    public Workspace(String name, MODE privacy, float quota){
+        _name = name;
+        _privacy = privacy;
+        _quota = quota;
+    }
+
+    /*********************************/
+    /****** GETTERS AND SETTERS ******/
+    /*********************************/
+
+    /* Name */
+    public String getName() {
+        return _name;
+    }
+
+    /* User List */
+    public ArrayList<String> getUserList() {
+        return _users;
+    }
+
+    /* Public Profile */
+    public ArrayList<String> getPublicProfile() {
+        return _tags;
+    }
+
+    /* Privacy */
+    public MODE getPrivacy() {
+        return _privacy;
+    }
+
+    public void setPrivacy(MODE privacy) {
+        _privacy = privacy;
+    }
+
+    /* Quota */
+    public float getQuota() {
+        return _quota;
+    }
+
+    public void setQuota(float quota) {
+        // TODO - Possivelmente necessário completar esta informação
+        _quota = quota;
+    }
+
+    /*********************************/
+    /******** LIST MANAGEMENT ********/
+    /*********************************/
+
+    /* User List */
+    public boolean removeUser(String id){
+        return _users.remove(id);
+    }
+
+    public boolean addUser(String id){
+        try{
+            return _users.add(id);
+        } catch (Exception e){
+            Log.e("Workspace", "Can't add user. Had " + e.toString());
+            return false;
+        }
+    }
+
+    /* Public Profile */
+    //region DEPRECATED
+    // Seems like it's deprecated :P
+    public boolean addTag(String tag){
+        try{
+            return _tags.add(tag);
+        } catch (Exception e){
+            Log.e("User", "Can't add keyword. Had " + e.toString());
+            return false;
+        }
+    }
+
+    public boolean removeTag(String tag){
+        return _tags.remove(tag);
+    }
+    //endregion
+}
