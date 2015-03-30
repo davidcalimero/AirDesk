aa46c93980359bfd13bdea279402d7fa7485a1ee
David Silva <david.d.silva@ist.utl.pt>
2015-03-30 16:58:06 +0100

    utils class with byte <-> object methods

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/other/Utils.java
@@ -0,0 +1,61 @@
+package pt.ulisboa.tecnico.cmov.airdesk.other;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+public class Utils {
+
+    public static byte[] objectoToByteArray(Object object){
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = null;
+        byte[] bytes = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(object);
+            bytes = bos.toByteArray();
+
+        } catch (Exception e) {
+            Log.e("Utils", "Error transforming object into byte[]");
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException ex) {}
+            try {
+                bos.close();
+            } catch (IOException ex) {}
+        }
+        return bytes;
+    }
+
+    public static Object byteArrayToObject(byte[] bytes) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInput in = null;
+        Object object = null;
+        try {
+            in = new ObjectInputStream(bis);
+            object = in.readObject();
+        } catch (Exception e) {
+            Log.e("Utils", "Error transforming byte[] into object");
+        } finally {
+            try {
+                bis.close();
+            } catch (IOException ex) {}
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {}
+        }
+        return object;
+    }
+}
