3d60021485823947476151e709c98bb22c0fea7c
David Silva <david.d.silva@ist.utl.pt>
2015-05-11 17:04:39 +0100

    MyAsyncTask v2

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/utility/MyAsyncTask.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/utility/MyAsyncTask.java
@@ -6,29 +6,53 @@ import android.os.Message;
 
 import java.io.Serializable;
 
-public abstract class MyAsyncTask<T extends Serializable> extends Handler {
+public abstract class MyAsyncTask<BackgroundType, ProgressType extends Serializable, PostType extends Serializable> extends Handler {
 
-    private static String MESSAGE = "message";
+    private static String POST_EXECUTE = "postExecute";
+    private static String PROGRESS_UPDATE = "progressUpdate";
+
+    private Thread thread;
 
     @Override
     public void handleMessage(Message msg) {
-        onPostExecute((T) msg.getData().getSerializable(MESSAGE));
+        Serializable data = msg.getData().getSerializable(POST_EXECUTE);
+        if(data != null)
+            onPostExecute((PostType) data);
+
+        data = msg.getData().getSerializable(PROGRESS_UPDATE);
+        if(data != null)
+            onProgressUpdate((ProgressType) data);
+}
+
+    protected abstract PostType doInBackground(BackgroundType param);
+
+    protected abstract void onProgressUpdate(ProgressType param);
+
+    protected abstract void onPostExecute(PostType param);
+
+    public void publishProgress(ProgressType param){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(PROGRESS_UPDATE, param);
+        Message msg = obtainMessage();
+        msg.setData(bundle);
+        sendMessage(msg);
     }
 
-    protected abstract T doInBackground();
+    public void cancel(){
+        thread.interrupt();
+    }
 
-    protected abstract void onPostExecute(T result);
-
-    public static void start(final MyAsyncTask<Serializable> runnable){
-        new Thread() {
+    public void execute(final BackgroundType param){
+        thread = new Thread() {
             @Override
             public void run() {
                 Bundle bundle = new Bundle();
-                bundle.putSerializable(MESSAGE, runnable.doInBackground());
-                Message msg = runnable.obtainMessage();
+                bundle.putSerializable(POST_EXECUTE, doInBackground(param));
+                Message msg = obtainMessage();
                 msg.setData(bundle);
-                runnable.sendMessage(msg);
+                sendMessage(msg);
             }
-        }.start();
+        };
+        thread.start();
     }
 }
